bf8f021d3d173fb6a6d048daadc8953d5bc6111b
L.Difino <125497687+TheTruth96@users.noreply.github.com>
2023-04-06 17:56:48 +0200

    Add files via upload

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ldifino <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 19:56:53 by ldifino           #+#    #+#             */
-/*   Updated: 2023/04/05 20:21:16 by ldifino          ###   ########.fr       */
+/*   Updated: 2023/04/06 17:55:27 by ldifino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!*lst)
-	{
-		*lst = new;
+	if (!lst || !new)
 		return ;
+	if (*lst)
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
 	}
-	last = ft_lstlast(*lst);
-	last->next = new;
+	else
+		*lst = new;
 }
